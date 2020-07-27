fcf792f88437c430295b43bb17b0d54f3a529f59
isaac stott <isaac.cloyd@gmail.com>
2020-07-27 17:01:05 -0600

    solved robot

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -107,32 +107,48 @@ class SortingRobot:
         #5. once their is a loop over the list with nothing to sort. break the loop. 
         #6. return sorted list. 
         # self.set_light_off()
+        self.set_light_on()
 
-
-        while not self.light_is_on:
+        while self.light_is_on():
             self.set_light_off()
 
+            while self.can_move_right():
 
-            if self.compare_item() == -1:
-                self.swap_item()
-                if self.can_move_right() == True:
-                    self.move_right()
-                else:
-                    self.move_left()
+                if self.compare_item() == None:
+                    if self.can_move_right() == True:
+                        self.swap_item()
+                        self.move_right()
+                    else:
+                        self.move_right()
 
-            if self.compare_item() == 1:
-                if self.can_move_right() == True:
-                    self.move_right()
-                else:
-                    self.move_left()
+                if self.compare_item() == -1: #position is greater than item.
+                    if self.can_move_left() == True:
+                        self.move_left()
+                    else:
+                        self.swap_item()
+                        self.move_right()
 
-            if self.compare_item() == None:
-                if self.can_move_right() == True:
-                    self.move_right()
-                else:
-                    self.move_left()
-            else:
-                self.set_light_on()
+                if self.compare_item() == 1: #item is greater than position.
+                    if self.can_move_left() == True:
+                        self.set_light_on()
+                        self.swap_item()
+                        self.move_left()
+                    else:
+                        self.move_right()
+
+                if self.compare_item() == 0:
+                    if self.can_move_left() == True:
+                        self.move_left()
+                    else:
+                        self.move_right()
+            
+            while self.can_move_left():
+                self.move_left()
+
+            
+            
+ 
+
             
 
 
@@ -140,6 +156,7 @@ class SortingRobot:
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
+    # arr = [3, 5, 8, 0]
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
@@ -156,3 +173,16 @@ if __name__ == "__main__":
             #     if self._list[i] > self._list[i + 1]:
             #         self.set_light_on
             #         self.swap_item
+
+                #     while self.compare_item() == None:
+
+                # if self.can_move_left() == True:
+                #     if self.can_move_right() == True:
+                #         self.swap_item()
+                #         self.move_left()
+                # elif self.can_move_left() == False:
+                #     self.swap_item()
+                #     self.move_right()
+                # else:
+                #     if self.can_move_right() == False:
+                #         self.set_light_off()
